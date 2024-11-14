784c484df05317f62a02f1aae5a9d6799d0a1894
Lucas Flaquer <lucas.flaquer@gmail.com>
2024-11-14 16:52:11 -0300

    setup theme

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,11 @@
 export function Home() {
-  return <div className="flex items-center text-2xl">Helo</div>
+  return (
+    <div className="flex items-center text-2xl">
+      <h1 className="font-title">Hi!</h1>
+      <p className="font-text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque,
+        impedit!
+      </p>
+    </div>
+  )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,28 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      'yellow-dark': '#C47F17',
+      yellow: '#DBAC2C',
+      'yellow-light': '#F1E9C9',
+      'purple-dark': '#4B2995',
+      purple: '#8047F8',
+      'purple-light': '#EBE5F9',
+      'base-title': '#272221',
+      'base-subtitle': '#403937',
+      'base-text': '#574F4D',
+      'base-label': '#8D8686',
+      'base-hover': '#D7D5D5',
+      'base-button': '#E6E5E5',
+      'base-input': '#EDEDED',
+      'base-card': '#F3F2F2',
+      background: '#FAFAFA',
+      white: '#FFFFFF',
+    },
+    fontFamily: {
+      text: ['Roboto', 'serif'],
+      title: ['"Baloo 2"', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
