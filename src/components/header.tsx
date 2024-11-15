e5e1838aee23631662430ee4c595c9a498c7db9b
Lucas Flaquer <lucas.flaquer@gmail.com>
2024-11-15 18:04:08 -0300

    coffees

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,14 +4,14 @@ import Logo from '../assets/Logo.svg'
 
 export function Header() {
   return (
-    <header className="bg-background flex justify-between px-40 py-8">
+    <header className="flex justify-between bg-background px-40 py-8">
       <img src={Logo} alt="Coffee delivery" />
       <div className="flex gap-3">
-        <div className="bg-purple-light text-purple-dark flex items-center rounded-lg p-2">
-          <MapPin size={22} weight="fill" className="text-purple mr-1" />
+        <div className="flex items-center rounded-lg bg-purple-light p-2 text-purple-dark">
+          <MapPin size={22} weight="fill" className="mr-1 text-purple" />
           <span className="font-text text-sm">Porto Alegre, RS</span>
         </div>
-        <div className="bg-yellow-light flex items-center rounded-lg p-2">
+        <div className="flex items-center rounded-lg bg-yellow-light p-2">
           <button>
             <ShoppingCart
               weight="fill"
