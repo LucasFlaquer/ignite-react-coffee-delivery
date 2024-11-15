daa11b23846e84266adfed90a5cf4be24c74d8a4
Lucas Flaquer <lucas.flaquer@gmail.com>
2024-11-15 14:03:44 -0300

    page home

--- src/components/coffees-list.tsx
+++ src/components/coffees-list.tsx
@@ -4,8 +4,8 @@ import { CoffeeItem } from './coffee-item'
 export function CoffeesList() {
   return (
     <section className="px-40">
-      <h2>Nossos cafés</h2>
-      <div className="mb-10 mt-14 grid grid-cols-4 gap-x-8 gap-y-10">
+      <h2 className="font-title text-4xl font-extrabold">Nossos cafés</h2>
+      <div className="mb-[157px] mt-14 grid grid-cols-4 gap-x-8 gap-y-10">
         {coffees.map((coffee) => (
           <CoffeeItem coffee={coffee} key={coffee.id} />
         ))}
