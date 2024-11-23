d7791c07525f2b6ef77d5fefe2e024eb65c8cd66
Lucas Flaquer <lucas.flaquer@gmail.com>
2024-11-23 17:08:00 -0300

    resolve path

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,8 +1,8 @@
 import { createBrowserRouter } from 'react-router-dom'
 
 import { BaseLayout } from './layouts/base-layout'
+import { Checkout } from './pages/checkout'
 import { Home } from './pages/home'
-import { Checkout } from './pages/home/checkout'
 
 export const router = createBrowserRouter([
   {
