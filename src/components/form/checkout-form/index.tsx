65538733473713ebdc2471db9f1ea2f6cdd2f011
Lucas Flaquer <lucas.flaquer@gmail.com>
2024-11-26 20:59:34 -0300

    handle input autofill

--- src/components/form/checkout-form/index.tsx
+++ src/components/form/checkout-form/index.tsx
@@ -33,22 +33,35 @@ export function CheckoutForm() {
             <Input type="text" placeholder="CEP" {...register('cep')} />
           </div>
           <div className="area-address">
-            <Input type="text" placeholder="RUA" />
+            <Input type="text" placeholder="RUA" {...register('street')} />
           </div>
           <div className="area-number">
-            <Input type="text" placeholder="Número" />
+            <Input
+              type="text"
+              placeholder="Número"
+              {...register('streetNumber')}
+            />
           </div>
           <div className="area-complement">
-            <Input type="text" placeholder="Complemento" isOptional />
+            <Input
+              type="text"
+              placeholder="Complemento"
+              isOptional
+              {...register('complement')}
+            />
           </div>
           <div className="area-neighborhood">
-            <Input type="text" placeholder="Bairro" />
+            <Input
+              type="text"
+              placeholder="Bairro"
+              {...register('neighborhood')}
+            />
           </div>
           <div className="area-city">
-            <Input type="text" placeholder="Cidade" />
+            <Input type="text" placeholder="Cidade" {...register('city')} />
           </div>
           <div className="area-uf">
-            <Input type="text" placeholder="UF" />
+            <Input type="text" placeholder="UF" {...register('uf')} />
           </div>
         </div>
       </Card>
